c3b15898508719ce5a5d522e4348834265a97236
Valerii Onyshchenko <vonyshchenko@oroinc.com>
2025-05-03 00:11:04 +0300

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,7 +50,7 @@ module "eks" {
           policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
           access_scope = {
             type = "namespace"
-            namespaces = ["dev"]
+            namespaces = ["dev", "default"]
           }
         }
       }
